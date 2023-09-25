11b71a284fee71bbc6321205b5af0479bcc5533f
Abd ElRahman Khalifa <88741327+Abd002@users.noreply.github.com>
2023-09-25 17:20:58 +0300

    Update data.cpp

--- data.cpp
+++ data.cpp
@@ -117,7 +117,7 @@ for(auto it:s)cout<<it<<' ';
 
 
 //a=factorila[a], b=MOD-2;
-ll binpow(ll a, ll b) {
+ll fp(ll a, ll b) {
     ll res = 1;
     a %= MOD;
     while (b)
@@ -945,4 +945,40 @@ void findCircle(int x1, int y1, int x2, int y2, int x3, int y3)
     // h,k,r
     x = h, y = k;
     rr = r;
+}
+
+
+
+const int N = 1e6 + 5;
+ll fact[N], inv[N];
+ll Mod = 998244353;
+ll fp(ll base, ll exp)
+{
+    if (exp == 0)
+        return 1;
+    ll ans = fp(base, exp / 2);
+    ans = (ans * ans) % Mod;
+    if (exp % 2 != 0)
+        ans = (ans * (base % Mod)) % Mod;
+    return ans;
+}
+void build(ll n)
+{
+    fact[0] = 1;
+    inv[0] = 1;
+    for (ll i = 1; i <= n; i++)
+    {
+        fact[i] = (i * fact[i - 1]) % Mod;
+        inv[i] = fp(fact[i], Mod - 2);
+    }
+}
+int nCr(ll n, ll r)
+{
+    if(r>n)return 0;
+    if(!r)return 1;
+    return (((fact[n] * inv[n - r]) % Mod) * inv[r]) % Mod;
+}
+int nPr(ll n, ll r)
+{
+    return (fact[n] * inv[n - r]) % Mod;
 }
